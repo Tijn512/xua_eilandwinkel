71e2601b2fa0b59b56732d78c96e4eda6267b685
Tijn512 <107409950+Tijn512@users.noreply.github.com>
2022-09-01 15:04:54 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Made by Xua (TIJN)
+
+Eilandwinkel is gemakkelijk voor servers, deze eilandwinkel beschikt over ammoclips en tiewraps
+
+dit is allemaal aanpasbaar in de config wat je erin wilt!!
---
+++ config.lua
@@ -0,0 +1,47 @@
+Config = {}
+Config.DrawDistance = 7.5
+Config.MarkerSize = {x = 1.1, y = 0.7, z = 1.1}
+Config.MarkerType =  29
+Config.MarkerColor = {r = 50, g = 200, b = 50, a = 200}
+Config.Locale = 'en'
+
+Config.Zones = {
+
+	eilandwinkel = {
+		Items = {
+			{
+				name = "ammopistol",
+				label = "Pistol Clip",
+				price = 10000
+			},
+			{
+				name = "ammoshotgun",
+				label = "Shotgun Clip",
+				price = 15000
+			},
+			{
+				name = "ammorifle",
+				label = "Rifle Clip",
+				price = 20000
+			},
+			{
+				name = "ammosmg",
+				label = "Smg Clip",
+				price = 15000
+			},
+			{
+				name = "tiewrap",
+				label = "Tiewrap Handboeien",
+				price = 300
+			}
+		},
+		Pos = {
+			vector3(4428.20, -4450.06, 7.24)
+	},
+	Size  = 0.8,
+	Type  = 59,
+	Color = 25,
+	ShowBlip = false,
+  ShowMarker = true
+},
+}
